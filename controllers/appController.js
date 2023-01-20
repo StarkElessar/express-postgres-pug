18ea7826da34857a63a873af6f5c036e02b21774
Сергей <serj.elessar@gmail.com>
2023-01-21 00:27:54 +0300

    feat: контролер приложения для фронта

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,8 +1,25 @@
+const { User } = require('./../models')
+
 class AppController {
   getIndexPage(req, res) {
     res.render('pages/index', {
       pageTitle: 'Проверка работы!'
     })
+  }
+
+  createUser(req, res) {
+    res.render('pages/createUser')
+  }
+
+  async allUsers(req, res) {
+    const users = await User.findAll()
+    const allUsers = []
+
+    for (const { dataValues } of users) {
+      allUsers.push(dataValues)
+    }
+    
+    res.render('pages/allUsers', { users: allUsers })
   }
 }
 
