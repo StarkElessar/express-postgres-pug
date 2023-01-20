8f0848cb60a610459e340c62bd5c9a67a7797ed3
Сергей <serj.elessar@gmail.com>
2023-01-21 00:30:47 +0300

    refactor: update script

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,7 @@
 import '../scss/styles.scss'
 
-import restApi from './modules'
+import createUser from './modules'
 
 document.addEventListener('DOMContentLoaded', () => {
-  class Home {
-    logger(string) {
-      console.log(`Page loaded ${string}`)
-    }
-  }
-
-  const home = new Home()
-  
-  home.logger('on port 3000')
-  console.log('Hot Reload...')
-  restApi()
+  createUser()
 })
