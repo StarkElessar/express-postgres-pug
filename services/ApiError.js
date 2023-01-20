38669165b1942e76ce357054095d91242b81a946
Сергей <serj.elessar@gmail.com>
2023-01-21 00:29:27 +0300

    feat: обработка ошибок

---
+++ services/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(status, messages) {
+    super()
+    this.status = status
+    this.message = messages
+  }
+
+  static badRequest(message) {
+    return new ApiError(400, message)
+  }
+  
+  static internal(message) {
+    return new ApiError(500, message)
+  }
+  
+  static forbidden(message) {
+    return new ApiError(403, message)
+  }
+}
+
+module.exports = ApiError
