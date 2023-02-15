2b7bb830677760150fe89431d60344475e8e3c58
Nikita Suyazov <jumper211212@gmail.com>
2023-02-15 02:18:10 -0800

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,16 +1,22 @@
-import { DataTypes } from 'sequelize'
+import {
+  DataTypes: {
+    BOOLEAN,
+    INTEGER,
+    STRING,
+  },
+} from 'sequelize';
 import sequelize from './../dp.js'
 
 const User = sequelize.define('user', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  login: { type: DataTypes.STRING, allowNull: false },
-  firstName: { type: DataTypes.STRING, allowNull: true },
-  lastName: { type: DataTypes.STRING, allowNull: true },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-  isActivated: { type: DataTypes.BOOLEAN, defaultValue: false },
-  activationLink: DataTypes.STRING,
-  isAdmin: { type: DataTypes.BOOLEAN, defaultValue: false },
+  id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+  login: { type: STRING, allowNull: false },
+  firstName: { type: STRING, allowNull: true },
+  lastName: { type: STRING, allowNull: true },
+  email: { type: STRING, unique: true, allowNull: false },
+  password: { type: STRING, allowNull: false },
+  isActivated: { type: BOOLEAN, defaultValue: false },
+  activationLink: { type: STRING },
+  isAdmin: { type: BOOLEAN, defaultValue: false },
 })
 
 export default User
